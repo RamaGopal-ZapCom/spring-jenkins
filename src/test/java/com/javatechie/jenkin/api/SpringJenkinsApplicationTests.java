3cbe13c1c20cf352111db84ed202485ef61edf15
RamaGopal-ZapCom <rama.gopal@zapcg.com>
2025-09-23 16:52:58 +0530

    bug fixed

--- src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
@@ -20,7 +20,8 @@ public class SpringJenkinsApplicationTests {
 		logger.info("Test case executing...");
 		logger.info("Test case executing...Second long is started");
 		logger.info("Test case executing...Second long is started 23-09-2025");
-		assertEquals(true, true)
+		logger.info("Test case executing...Second long is started 23-09-2025--------");
+		assertEquals(true, true);
 	}
 
 }
