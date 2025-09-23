1d550033070e39c7043c90eaebc3b1297c552ac7
RamaGopal-ZapCom <rama.gopal@zapcg.com>
2025-09-23 20:26:51 +0530

    updated

--- src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
+++ src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
@@ -2,14 +2,19 @@ package com.javatechie.jenkin.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class SpringJenkinsApplication {
+public class SpringJenkinsApplication extends SpringBootServletInitializer {
 
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringJenkinsApplication.class);
+	}
 	@GetMapping("/ping")
 	public String message() {
 		return "Wao!! Application Deployed successfully in SAP Cloud..";
